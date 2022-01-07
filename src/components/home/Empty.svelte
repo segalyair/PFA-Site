f416f5d815d0d8b7844ae053c5659c603789b74d
segalyair <segalyair11@gmail.com>
2022-01-07 14:24:12 +0200

    pointer helper

--- src/components/home/Empty.svelte
+++ src/components/home/Empty.svelte
@@ -2,10 +2,15 @@
     export let background = "#444";
 </script>
 
-<div class="container" style={`background:${background}`} />
+<div class="container" style={`background:${background}`}>
+    <img width="100" src="./finger-pointer.png" alt="Finger pointer" />
+</div>
 
 <style>
     .container {
+        display: flex;
+        justify-content: center;
+        align-items: center;
         height: 100%;
     }
 </style>
